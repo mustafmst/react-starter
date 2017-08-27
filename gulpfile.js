d3938314f1cacd1d0df2ab0669944e7bbaa972e1
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-08-27 21:27:55 +0200

    gulp tasks for sass and copy dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
-        webpack = require('webpack-stream');
+        webpack = require('webpack-stream')
+        sass = require('gulp-sass');
 
 gulp.task('webpack', function () {
     return gulp.src([
@@ -21,3 +22,38 @@ gulp.task('webpack', function () {
     }))
     .pipe(gulp.dest('./src/public'));
 });
+
+gulp.task('sass', function () {
+    return gulp.src([
+        './node_modules/bootstrap/scss/bootstrap.scss',
+        './src/scss/**/*.scss'
+    ])
+    .pipe(sass())
+    .pipe(gulp.dest('./src/public/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/popper.js/dist/umd/popper.min.js'
+    ])
+    .pipe(gulp.dest('./src/public/js'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src([
+        './node_modules/font-awesome/fonts/*'
+    ])
+    .pipe(gulp.dest('./src/public/fonts'));
+});
+
+gulp.task('fa', function () {
+    return gulp.src([
+        './node_modules/font-awesome/css/font-awesome.min.css'
+    ])
+    .pipe(gulp.dest('./src/public/css'));
+});
+
+gulp.task('default',['webpack','sass','js','fonts','fa']);
+
