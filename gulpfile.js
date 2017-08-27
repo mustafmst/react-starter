913ef3abe3a2a06ff125a5fcc67616b7dbfa750b
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-08-27 21:53:09 +0200

    just nice touch for starter

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,71 @@
-const gulp = require('gulp')
-        webpack = require('webpack-stream')
-        sass = require('gulp-sass');
+const gulp = require('gulp'),
+    webpack = require('webpack-stream'),
+    browserSync = require('browser-sync'),
+    sass = require('gulp-sass');
 
 gulp.task('webpack', function () {
     return gulp.src([
-        './src/app/**/*.jsx'
-    ])
-    .pipe(webpack({
-        module:{
-            loaders:[{
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query:{
-                    presets: ['es2015','react']
-                }
-            }]
-        },
-        output: {
-            filename: 'app.js'
-        }
-    }))
-    .pipe(gulp.dest('./src/public'));
+            './src/app/**/*.jsx'
+        ])
+        .pipe(webpack({
+            module: {
+                loaders: [{
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        presets: ['es2015', 'react']
+                    }
+                }]
+            },
+            output: {
+                filename: 'app.js'
+            }
+        }))
+        .pipe(gulp.dest('./src/public'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('sass', function () {
     return gulp.src([
-        './node_modules/bootstrap/scss/bootstrap.scss',
-        './src/scss/**/*.scss'
-    ])
-    .pipe(sass())
-    .pipe(gulp.dest('./src/public/css'));
+            './node_modules/bootstrap/scss/bootstrap.scss',
+            './src/scss/**/*.scss'
+        ])
+        .pipe(sass())
+        .pipe(gulp.dest('./src/public/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('js', function () {
     return gulp.src([
-        './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './node_modules/jquery/dist/jquery.min.js',
-        './node_modules/popper.js/dist/umd/popper.min.js'
-    ])
-    .pipe(gulp.dest('./src/public/js'));
+            './node_modules/bootstrap/dist/js/bootstrap.min.js',
+            './node_modules/jquery/dist/jquery.min.js',
+            './node_modules/popper.js/dist/umd/popper.min.js'
+        ])
+        .pipe(gulp.dest('./src/public/js'));
 });
 
 gulp.task('fonts', function () {
     return gulp.src([
-        './node_modules/font-awesome/fonts/*'
-    ])
-    .pipe(gulp.dest('./src/public/fonts'));
+            './node_modules/font-awesome/fonts/*'
+        ])
+        .pipe(gulp.dest('./src/public/fonts'));
 });
 
 gulp.task('fa', function () {
     return gulp.src([
-        './node_modules/font-awesome/css/font-awesome.min.css'
-    ])
-    .pipe(gulp.dest('./src/public/css'));
+            './node_modules/font-awesome/css/font-awesome.min.css'
+        ])
+        .pipe(gulp.dest('./src/public/css'));
 });
 
-gulp.task('default',['webpack','sass','js','fonts','fa']);
+gulp.task('serve',['webpack', 'sass', 'js', 'fonts', 'fa'], function () {
+    browserSync.init({
+        server: './src'
+    });
 
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/app/**/*.jsx', ['webpack']);
+    gulp.watch('./src/index.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['serve']);
--- src/app/app.jsx
+++ src/app/app.jsx
@@ -4,7 +4,11 @@ import ReactDom from 'react-dom';
 class App extends Component{
     render(){
         return (
-            <h1>Hello World!</h1>
+            <div>
+                <div className="container text-center">
+                    <h1 className="display-3 col-md-9 mx-auto mt-5">Welcome to React Starter Project</h1>
+                </div>
+            </div>
         );
     }
 }
